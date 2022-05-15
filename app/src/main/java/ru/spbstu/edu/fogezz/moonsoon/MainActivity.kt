14e46f2814a43dfeb2a211bcad6a19e98bd69068
Daniil Sud <danya9877@gmail.com>
2022-05-15 17:35:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/spbstu/edu/fogezz/moonsoon/MainActivity.kt
@@ -0,0 +1,42 @@
+package ru.spbstu.edu.fogezz.moonsoon
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+class MainActivity : AppCompatActivity() {
+
+    private var editText: EditText? = null
+    private var listView: ListView? = null
+    private var stringMessage: String? = null
+    private var cipher: Cipher? = null
+    private var decipher: Cipher? = null
+    private var secretKeySpec: SecretKeySpec? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.editText)
+        listView = findViewById(R.id.listView)
+    }
+
+     fun client(): Socket = runBlocking {
+         return@runBlocking aSocket(ActorSelectorManager(Dispatchers.IO)).tcp()
+             .connect("localhost", 8081)
+    }
+
+    fun sendButton(view: View?) {
+        val date = Date()
+        editText!!.setText("")
+    }
+}
